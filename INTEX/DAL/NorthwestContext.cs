6ba61b00bc52a62ce70f3169ffa275fa69af25d4
Adam.mautz <Adam.mautz@gmail.com>
2019-12-03 14:43:10 -0700

    made a DAL folder and context file. Added connection string info. Created customer model

---
+++ INTEX/DAL/NorthwestContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.DAL
+{
+    public class NorthwestContext
+    {
+    }
+}
---
+++ INTEX/Models/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace INTEX.Models
+{
+    [Table("Customer")]
+    public class Customer
+    {
+        [Key]
+        public int CustID { get; set; }
+
+        public string Cust_First_Name { get; set; }
+
+        public string Cust_Last_Name { get; set; }
+
+        public string Cust_Street_Address1 { get; set; }
+
+        public string Cust_Street_Address2 { get; set; }
+
+        public string Cust_City { get; set; }
+
+        public string Cust_State { get; set; }
+
+        public string Cust_Zip { get; set; }
+
+        public string Cust_Zip_Plus { get; set; }
+
+        public string Cust_Area_Code { get; set; }
+
+        public string Cust_Phone { get; set; }
+
+        public string Cust_Email { get; set; }
+
+        public string Cust_Username { get; set; }
+
+        public string Cust_Password { get; set; }
+
+
+    }
+}
