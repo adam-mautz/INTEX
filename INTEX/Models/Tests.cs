1d6a7018820e8de43e4aaaa157f379300c282026
McKay <55812850+macabryson@users.noreply.github.com>
2019-12-05 17:23:21 -0700

    styling mostly

--- INTEX/Models/Tests.cs
+++ INTEX/Models/Tests.cs
@@ -11,22 +11,31 @@ namespace INTEX.Models
     public class Tests
     {
         [Key]
+        [Display(Name = "Sequence Code")]
         public int Compound_Sequence_Code { get; set; }
 
+        [Display(Name = "LT Number")]
         public int LT_Number { get; set; }
 
+        [Display(Name = "Test Type")]
         public string Test_Type { get; set; }
 
+        [Display(Name = "Comments")]
         public string Comment { get; set; }
 
+        [Display(Name = "Status")]
         public string StatusID { get; set; }
 
+        [Display(Name = "Test Data File")]
         public string Raw_Results { get; set; } //this will be holding a text file. Will the data type need to be changed?
 
+        [Display(Name = "Approved")]
         public Boolean? Approved { get; set; }
 
+        [Display(Name = "Data Scheduled")]
         public DateTime? Test_Date_Scheduled { get; set; }
 
+        [Display(Name = "Order ID")]
         public int OrderID { get; set; }
 
 
